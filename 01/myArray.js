375a64378a43249f7c70cc23720139721566e2b3
Md. Naimur Rahman Naim <naimur2210026@gmail.com>
2025-10-17 22:59:07 +0600

    learned array syntax

---
+++ 01/myArray.js
@@ -0,0 +1,13 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var superHeroes = [];
+var heroPower = []; // same type 2
+var heroPower2 = []; // same type 2
+var allUsers = [];
+superHeroes.push("Khalid");
+heroPower.push(1, 2, 3);
+allUsers.push({ name: "Naim", isActive: true });
+var MLModels = [
+  [255, 255, 259],
+  [1, 2, 3.4],
+];
---
+++ 01/myArray.ts
@@ -0,0 +1,22 @@
+const superHeroes: string[] = [];
+const heroPower: number[] = []; // same type 2
+const heroPower2: Array<number> = []; // same type 2
+
+type User = {
+  name: string;
+  isActive: boolean;
+};
+
+const allUsers: User[] = [];
+
+superHeroes.push("Khalid");
+heroPower.push(1, 2, 3);
+
+allUsers.push({ name: "Naim", isActive: true });
+
+const MLModels: number[][] = [
+  [255, 255, 259],
+  [1, 2, 3.4],
+];
+
+export {};
