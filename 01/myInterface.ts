eb3a11366d9a78cb3386d5496170edcc3f026454
Md. Naimur Rahman Naim <naimur2210026@gmail.com>
2025-10-19 23:38:58 +0600

    type and tuple extra code adced

---
+++ 01/myInterface.ts
@@ -0,0 +1,56 @@
+interface User {
+  readonly _id: number;
+  email: string;
+  userId: number;
+  googleId?: string;
+  startTrial: () => string;
+  startTrial2(): number;
+  getCoupon(couponName: string, discount: number): number;
+}
+
+interface User {
+  // allowed for adding more vars
+  email: string; // double value is allowed as well
+  githubToken: string;
+}
+
+// inheritance
+interface Admin extends User {
+  role: "admin" | "ta" | "viewer";
+}
+
+const naim: User = {
+  _id: 2349,
+  githubToken: "gituser",
+  email: "afh@kisuakta.com",
+  userId: 26,
+  startTrial: () => {
+    return "trial started";
+  },
+  startTrial2() {
+    return 2;
+  },
+  getCoupon: (name: "naim10", off: 10) => {
+    return 7;
+  },
+};
+
+const naim2: Admin = {
+  _id: 2349,
+  githubToken: "gituser",
+  email: "afh@kisuakta.com",
+  userId: 26,
+  startTrial: () => {
+    return "trial started";
+  },
+  startTrial2() {
+    return 2;
+  },
+  getCoupon: (name: "naim10", off: 10) => {
+    return 7;
+  },
+  role: "ta",
+};
+
+naim.email = "sdkjf@kjhsd.com";
+// naim._id = 245467; // it's readonly
--- 01/myTupEnums.ts
+++ 01/myTupEnums.ts
@@ -13,10 +13,34 @@ User2 = ["ch", 5];
 // strict type
 let rgb: [number, number, number] = [255, 122, 101]; // colors should be within 3 values
 
+// inheritance
+
 type User3 = [string, string];
+
+type User4 = User3 & {
+  roll: number;
+};
 
 const newUser: User3 = ["Naim", "asdg@foogle.com"];
 
 newUser[1] = "ab@hoggle.com";
 
-// newUser.push(true); // can't be pushed
+const newUser2: User4 = ["Naim", "asdg@foogle.com"] as User4;
+
+newUser2.roll = 28197;
+
+type User3Object = {
+  name: string;
+  email: string;
+};
+
+// Now, 'inheritance' via intersection works perfectly!
+type User4Object = User3Object & {
+  roll: number;
+};
+
+const newUser3: User4Object = {
+  name: "Naim",
+  email: "asdg@foogle.com",
+  roll: 234256,
+};
