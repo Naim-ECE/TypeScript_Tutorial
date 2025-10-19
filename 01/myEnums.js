10c5935c0b969bcd72b7e32a172f45be19711652
Md. Naimur Rahman Naim <naimur2210026@gmail.com>
2025-10-19 19:02:47 +0600

    learned enums

---
+++ 01/myEnums.js
@@ -0,0 +1,15 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var hcSeat = "aisle"; /* seatChoice.AISLE */
+
+// without enum const
+
+// Object.defineProperty(exports, "__esModule", { value: true });
+// var seatChoice;
+// (function (seatChoice) {
+//     seatChoice["AISLE"] = "aisle";
+//     seatChoice[seatChoice["MIDDLE"] = 21] = "MIDDLE";
+//     seatChoice[seatChoice["WINDOW"] = 22] = "WINDOW";
+//     seatChoice[seatChoice["FOURTH"] = 40] = "FOURTH";
+// })(seatChoice || (seatChoice = {}));
+// var hcSeat = seatChoice.AISLE;
---
+++ 01/myEnums.ts
@@ -0,0 +1,10 @@
+const enum seatChoice {
+  AISLE = "aisle",
+  MIDDLE = 21, // numbers are auto generated and if a string is added then next value needs to be string to fulfill the order or it can also be  a number then it will add to the next by default
+  WINDOW,
+  FOURTH = 40,
+}
+
+const hcSeat = seatChoice.AISLE;
+
+export {};
